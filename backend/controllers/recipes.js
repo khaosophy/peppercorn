8f52d96d397925846ad32280b2b3606820aa4c21
Casey James <casey@khaosophy.com>
2020-07-13 15:00:36 -0400

    recipe API: update and delete

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,4 +1,5 @@
 const Recipe = require('../models/Recipe');
+const { findByIdAndUpdate } = require('../models/Recipe');
 
 // @desc      Get all recipes for user
 // @route     GET /api/v1/recipes
@@ -56,19 +57,48 @@ exports.createRecipe = async (req, res, next) => {
 // @desc      Update recipe based on ID
 // @route     PUT /api/v1/recipes/:id
 // @access    Public
-exports.updateRecipe = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: 'update recipe',
-  });
+exports.updateRecipe = async (req, res, next) => {
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if(!recipe) {
+      return res.status(400).json({ 
+        success: false,
+        message: 'Recipe not found',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: recipe,
+    })
+  } catch (err) {
+    res.status(400).json({ 
+      success: false,
+      message: 'Uncaught error',
+    });
+  }
 }
 
 // @desc      Delete recipe based on ID
 // @route     DELETE /api/v1/recipes/:id
 // @access    Public
-exports.deleteRecipe = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: 'delete recipe',
-  });
+exports.deleteRecipe = async (req, res, next) => {
+  try {
+    const recipe = await Recipe.findByIdAndDelete(req.params.id);
+
+    if(!recipe) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  
 }
