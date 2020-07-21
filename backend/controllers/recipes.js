de1c4e2a3fe1f54d6c9aa2059d2316d4c498d1c7
Casey James <casey@khaosophy.com>
2020-07-21 10:20:17 -0400

    added asyncHandler to better handle bad API calls

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,104 +1,81 @@
 const Recipe = require('../models/Recipe');
-const { findByIdAndUpdate } = require('../models/Recipe');
+const asyncHandler = require('../middleware/async');
 
 // @desc      Get all recipes for user
 // @route     GET /api/v1/recipes
 // @access    Public
-exports.getRecipes = async (req, res, next) => {
-  try {
-    const recipes = await Recipe.find();
+exports.getRecipes = asyncHandler(async (req, res, next) => {
+  const recipes = await Recipe.find();
 
-    res.status(200).json({
-      success: true,
-      count: recipes.length,
-      data: recipes,
-    })
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-}
+  res.status(200).json({
+    success: true,
+    count: recipes.length,
+    data: recipes,
+  })
+});
 
 // @desc      Get single recipe based on ID
 // @route     GET /api/v1/recipes/:id
 // @access    Public
-exports.getRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
+exports.getRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findById(req.params.id);
 
-    if(!recipe) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: recipe,
-    })
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if(!recipe) {
+    return res.status(400).json({ success: false });
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: recipe,
+  })
+});
 
 // @desc      Create new recipe
 // @route     POST /api/v1/recipes
 // @access    Public
-exports.createRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.create(req.body);
+exports.createRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: recipe,
-    });
-  } catch(err) {
-    res.status(400).json({ success: false });
-  }
-}
+  res.status(201).json({
+    success: true,
+    data: recipe,
+  });
+});
 
 // @desc      Update recipe based on ID
 // @route     PUT /api/v1/recipes/:id
 // @access    Public
-exports.updateRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if(!recipe) {
-      return res.status(400).json({ 
-        success: false,
-        message: 'Recipe not found',
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: recipe,
-    })
-  } catch (err) {
-    res.status(400).json({ 
+  if(!recipe) {
+    return res.status(400).json({ 
       success: false,
-      message: 'Uncaught error',
+      message: 'Recipe not found',
     });
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: recipe,
+  })
+});
 
 // @desc      Delete recipe based on ID
 // @route     DELETE /api/v1/recipes/:id
 // @access    Public
-exports.deleteRecipe = async (req, res, next) => {
-  try {
-    const recipe = await Recipe.findByIdAndDelete(req.params.id);
+exports.deleteRecipe = asyncHandler(async (req, res, next) => {
+  const recipe = await Recipe.findByIdAndDelete(req.params.id);
 
-    if(!recipe) {
-      return res.status(400).json({ success: false });
-    }
+  if(!recipe) {
+    return res.status(400).json({ success: false });
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  
-}
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+})
