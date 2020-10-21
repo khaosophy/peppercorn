f0be68bed4b704284daa9edec573f43f39d42c1a
Casey James <casey@khaosophy.com>
2020-10-21 16:38:01 -0400

    added API route to get single ingredient

--- backend/controllers/ingredients.js
+++ backend/controllers/ingredients.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc      Get all ingredients
-// @route     GET /api/v1/ingredients
+// @route     GET /api/v1/ingredients/:id
 // @access    Public
 exports.getIngredients = asyncHandler(async (req, res, next) => {
   const ingredients = await Ingredient.find();
@@ -14,3 +14,16 @@ exports.getIngredients = asyncHandler(async (req, res, next) => {
     data: ingredients,
   });
 });
+
+exports.getSingleIngredient = asyncHandler(async (req, res, next) => {
+  const ingredient =  await Ingredient.findById(req.params.id);
+
+  if(!ingredient) {
+    return next(new ErrorResponse(`Ingredient not found with id of ${req.params.id}`))
+  }
+
+  res.status(200).json({
+    success: true,
+    data: ingredient,
+  });
+})
--- backend/routes/ingredients.js
+++ backend/routes/ingredients.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
   getIngredients,
+  getSingleIngredient,
 } = require('../controllers/ingredients');
 
 router.route('/')
   .get(getIngredients);
+
+router.route('/:id')
+  .get(getSingleIngredient);
 
 module.exports = router;
