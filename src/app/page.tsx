55fe5917d2abb1b2fdd46c26b6f720cb553e1b9a
Casey James Perno <casey@khaosophy.com>
2023-05-26 14:43:43 -0400

    use next Links in place of anchor elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h3>Whatcha wanna do?</h3>
       <ul>
-        <li>Already have an account? <a href="/login">Log in.</a></li>
-        <li>Want to get started? <a href="/register">Register now.</a></li>
+        <li>Already have an account? <Link href="/login">Log in.</Link></li>
+        <li>Want to get started? <Link href="/register">Register now.</Link></li>
       </ul>
     </main>
   )
