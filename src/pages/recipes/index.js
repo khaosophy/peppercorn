2cf9572853614fa0c3faded99d596280f211cf42
Casey James Perno <casey@khaosophy.com>
2023-05-29 12:35:05 -0400

    cleanup and add TODOs

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -3,11 +3,18 @@ import supabase from '../../lib/supabase';
 import Link from '../../components/Link';
 
 export default function Recipes() {
+  /**
+   * TODO:
+   * * fetch only recipes that the user created
+   *   * when saving, pass the user id (created_by) to the recipe
+   *   * see the Supabase permission templates
+   * * style the list of recipes
+   */
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const getRecipes = async () => {
-    let { data, error } = await supabase.from('recipes').select('*');
+    const { data, error } = await supabase.from('recipes').select('*');
 
       if (error) {
         /* TODO: display error */
