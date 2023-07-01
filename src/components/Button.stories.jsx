9b57c3fd935266cd12037f7313b9e0431f63b9db
Casey Perno <casey@khaosophy.com>
2023-06-30 22:29:43 -0400

    button story

--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -1,16 +1,21 @@
 import Button from './Button';
 
-/* todo: get this working, including the many state variants */
-
-const meta = {
+export default {
   title: 'Button',
   component: Button,
-};
+  argTypes: {
+    variant: { control: 'select', options: ['solid', 'outline'] },
+    color: { control: 'select', options: ['slate', 'blue', 'white'] },
+    shape: { control: 'select', options: ['square', 'rounded'] },
+  },
+}
 
-export default meta;
+export const Default = {
+  args: {
+    children: 'Click Me',
+    variant: 'solid',
+    color: 'slate',
+    shape: 'square',
+  },
+}
 
-export const Default = () => (
-  <Button>
-    Click Me
-  </Button>
-);
