018e0e098a061b49c4a29d0f94a1b9238b7e1c2f
Casey Perno <casey@khaosophy.com>
2023-07-01 19:49:34 -0400

    home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import routes from '../routes.config';
+import Hero from '../components/Hero';
+import Container from '../components/Container';
 
 export default function Home() {
   return (
-    <main className={clsx(
-      'max-w-6xl mx-auto',
-      'flex flex-1 flex-col justify-center',
-      'min-h-full py-4 px-2',
-    )}>
-      <h3>Whatcha wanna do?</h3>
-      <ul>
-        <li>Already have an account? <Link href={routes.login}>Log in.</Link></li>
-        <li>Want to get started? <Link href={routes.register}>Register now.</Link></li>
-        <li>Ready to add a recipe? <Link href={routes.newRecipe}>Get started.</Link></li>
-        <li>Looking for your recipes? <Link href={routes.recipes}>Check them out.</Link></li>
-      </ul>
+    <main>
+      <Hero />
+      <Container className="">
+        <h3>Whatcha wanna do?</h3>
+        <ul>
+          <li>Already have an account? <Link href={routes.login}>Log in.</Link></li>
+          <li>Want to get started? <Link href={routes.register}>Register now.</Link></li>
+          <li>Ready to add a recipe? <Link href={routes.newRecipe}>Get started.</Link></li>
+          <li>Looking for your recipes? <Link href={routes.recipes}>Check them out.</Link></li>
+        </ul>
+      </Container>
     </main>
   )
 }
