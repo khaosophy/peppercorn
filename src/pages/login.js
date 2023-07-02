1a5c2bf48a73f9b0b255feb73ac2f2c00deda444
Casey Perno <casey@khaosophy.com>
2023-07-01 20:11:34 -0400

    sticky footer

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,7 +27,7 @@ export default function Login() {
   }
 
   return (
-    <main className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Log in to your account
       </h2>
