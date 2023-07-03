1e5f584edfdde5aae0cda66b5e054deef818f774
Casey Perno <casey@khaosophy.com>
2023-07-02 22:39:15 -0400

    primary features and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import PrimaryFeatures from '@/components/PrimaryFeatures';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <PrimaryFeatures />
     </main>
   )
 }
