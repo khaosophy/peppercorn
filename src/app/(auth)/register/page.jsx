7a96c98435471277920b70dc3a3b251decd99904
Casey Perno <casey@khaosophy.com>
2023-07-07 16:00:12 -0400

    register page meta data

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,45 +1,15 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Head from 'next/head';
-
-import routes from '@/routes.config';
-import supabase from '@/lib/supabase';
 
 import Link from '@/components/Link';
-import Button from '@/components/Button';
-import TextField from '@/components/TextField';
 import Logo from '@/components/Logo';
+import RegisterForm from './RegisterForm';
+
+export const metadata = {
+  title: 'Sign Up | Peppercorn',
+};
 
 export default function Register() {
-  const router = useRouter();
-  const [ email, setEmail ] = useState('');
-  // TODO: password should be at least six characters long
-  const [ password, setPassword ] = useState('');
-  const [ firstName, setFirstName ] = useState('');
-  const [ lastName, setLastName ] = useState('');
-
-  const handleRegistration = async (e) => {
-    e.preventDefault();
-    console.log('registering...');
-
-    const { data, error } = await supabase.auth.signUp({ email, password });
-    /* TODO: save first and last name to database */
-
-    if (error) {
-      /* TODO: display error */
-      return console.error(error);
-    }
-
-    console.log(data);
-    router.push(routes.home);
-  }
-
   return (
     <>
-      <Head>
-        <title>Sign Up | TaxPal</title>
-      </Head>
       <div className="flex flex-col">
         <Link href="/" aria-label="Home">
           <Logo className="h-10 w-auto" width={110} height={40} />
@@ -60,65 +30,7 @@ export default function Register() {
           </p>
         </div>
       </div>
-      <form
-        className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
-        onSubmit={handleRegistration}
-      >
-        <TextField
-          label="First name"
-          id="first_name"
-          name="first_name"
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          autoComplete="given-name"
-          required
-        />
-        <TextField
-          label="Last name"
-          id="last_name"
-          name="last_name"
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          autoComplete="family-name"
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Email address"
-          id="email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="email"
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Password"
-          id="password"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="new-password"
-          required
-        />
-        <div className="col-span-full">
-          <Button
-            type="submit"
-            variant="solid"
-            color="blue"
-            className="w-full"
-          >
-            <span>
-              Sign up <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Button>
-        </div>
-      </form>
+      <RegisterForm />
     </>
   )
 }
