91d724c445dcbc19abfe2091f72cd75753c2af51
Casey Perno <casey@khaosophy.com>
2023-07-07 15:56:07 -0400

    login page meta data

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,40 +1,13 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import routes from '@/routes.config';
-import supabase from '@/lib/supabase';
 
 import Link from '@/components/Link';
-import Button from '@/components/Button';
-import TextField from '@/components/TextField';
 import Logo from '@/components/Logo';
+import LoginForm from './loginForm';
 
 export const metadata = {
-  /* todo: this isn't working? */
   title: 'Sign In | Peppercorn',
 };
 
-export default function Page() {
-  const router = useRouter();
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    console.log('logging in...');
-
-    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-
-    if (error) {
-      /* TODO: display error */
-      return console.error(error);
-    }
-
-    console.log(data);
-    router.push(routes.recipes);
-  }
-
+export default function Login() {
   return (<>
     <div className="flex flex-col">
       <Link href="/" aria-label="Home">
@@ -56,39 +29,6 @@ export default function Page() {
         </p>
       </div>
     </div>
-    <form className="mt-10 grid grid-cols-1 gap-y-8" onSubmit={handleLogin}>
-      <TextField
-        label="Email address"
-        id="loginEmail"
-        name="email"
-        type="email"
-        autoComplete="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <TextField
-        label="Password"
-        id="loginPassword"
-        name="password"
-        type="password"
-        autoComplete="current-password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <div>
-        <Button
-          type="submit"
-          variant="solid"
-          color="blue"
-          className="w-full"
-        >
-          <span>
-            Sign in <span aria-hidden="true">&rarr;</span>
-          </span>
-        </Button>
-      </div>
-    </form>
+    <LoginForm />
   </>)
 }
