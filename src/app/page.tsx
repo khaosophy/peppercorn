006ca8aab673dae0459e7255d78895e597c69650
Casey Perno <casey@khaosophy.com>
2023-07-19 15:39:56 -0400

    home page meta info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ import PrimaryFeatures from '@/components/PrimaryFeatures';
 import SecondaryFeatures from '@/components/SecondaryFeatures';
 import CallToAction from '@/components/CallToAction';
 
+export const metadata = {
+  title: 'Peppercorn | Meal planning made simple',
+  description: 'Peppercorn is the meal planning tool that helps you organize your food for the week with the ease and flexibility you want.',
+};
+
 export default function Home() {
   return (<>
     <Header />
