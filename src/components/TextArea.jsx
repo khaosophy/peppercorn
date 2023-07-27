64d044923863e76de85b860ae6cf736b0a1f4245
Casey Perno <casey@khaosophy.com>
2023-07-27 15:04:22 -0400

    text area component

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+
+export default function TextArea(props) {
+  const formClasses =
+    'block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm';
+
+  return (
+    <div className={props.className}>
+      <label
+        className="mb-2 block text-sm font-medium text-gray-700"
+        htmlFor={props.id}
+      >
+        {props.label}
+      </label>
+      <textarea
+        className={clsx(
+          formClasses,
+        )}
+        type={props.type}
+        id={props.id}
+        name={props.name}
+        value={props.value}
+        onChange={props.onChange}
+        rows={props.rows}
+        required={props.required}
+      />
+    </div>
+  );
+}
